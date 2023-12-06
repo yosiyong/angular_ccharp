f4c31cf3002b0f70f6e1200612aed5bb962d94c4
YongHun Lee <yosiyong@gmail.com>
2023-12-06 21:43:55 +0900

    03_Frontend Api request

--- API/Program.cs
+++ API/Program.cs
@@ -7,8 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddCors();
 var app = builder.Build();
+
+//Configure the HTTP request pipline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseHttpsRedirection();
 
